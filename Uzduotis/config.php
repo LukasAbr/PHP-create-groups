951d0115474dde67f8c5be16b00c246024fbea5d
Lukas Abromas <lukas.abromas@gmail.com>
2017-03-01 09:50:24 +0200

    no message

--- Uzduotis/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASS', '');
-define('DB_DBNAME', 'groups_website');
-
-$db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASS, DB_DBNAME);
--- Uzduotis/protection.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] == true) {
-} else { header ("Location: index.html");
-  }
-?>
